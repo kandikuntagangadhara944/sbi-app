4aea1bf7cfc36bfe41d339ed66829f5d6617403b
kandikuntagangadhara944 <kandikuntagangadhara944@gmail.com>
2023-08-08 21:30:34 +0530

    first commit

---
+++ src/main/java/com/tcs/sbi/UserReg.java
@@ -0,0 +1,12 @@
+package com.tcs.sbi;
+import in.ashokit.security.PwdSecurityService;
+public class UserReg {
+	
+	public static void main(String[] args)
+	{
+		PwdSecurityService pss = new PwdSecurityService();
+		String encode = pss.encode("ashokit");
+		System.out.println(encode);
+	}
+
+}
